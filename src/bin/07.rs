bc0a40c4901837ba28dd988701467fd2a3e9bfa0
user <user@nospam.com>
2023-12-08 23:46:48 +0100

    Half day 7

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,7 +1,13 @@
+// Wrong Vqlue
+// 249057120
+// 248409953
+// 245065331
+// 248179786
+
 use lazy_static::lazy_static;
 use std::{
-    cmp::{max, min},
     collections::HashMap,
+    iter::zip, fmt,
 };
 
 advent_of_code::solution!(7);
@@ -24,53 +30,76 @@ lazy_static! {
     ]);
 }
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone, Copy)]
-enum HandType {
-    HighCard(u32),
-    OnePair(u32),
-    TwoPair(u32, u32),
-    ThreeOfkind(u32),
-    FullHouse(u32, u32),
-    FourOfkind(u32, u32),
-    FiveOfkind(u32),
+#[derive(Debug,Clone, Eq, PartialEq, Ord, PartialOrd, Copy)]
+enum HandType<'a> {
+    HighCard(&'a str),
+    OnePair(&'a str),
+    TwoPair(&'a str),
+    ThreeOfkind(&'a str),
+    FullHouse(&'a str),
+    FourOfkind(&'a str),
+    FiveOfkind(&'a str),
 }
 
-fn compare_two_value(a1: u32, a2: u32, b1: u32, b2: u32) -> std::cmp::Ordering {
-    if max(a1, a2).cmp(&max(b1, b2)) == std::cmp::Ordering::Equal {
-        min(a1, a2).cmp(&min(b1, b2))
-    } else {
-        max(a1, a2).cmp(&max(b1, b2))
+impl fmt::Display for HandType<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {            
+            HandType::HighCard(v) => write!(f, "HighCard {}", *v),
+            HandType::OnePair(v) => write!(f, "OnePair {}", *v),
+            HandType::TwoPair(v) => write!(f, "TwoPair {}", *v),
+            HandType::ThreeOfkind(v) => write!(f, "ThreeOfkind {}", *v),
+            HandType::FullHouse(v) => write!(f, "FullHouse {}", *v),
+            HandType::FourOfkind(v) => write!(f, "FourOfkind {}", *v),
+            HandType::FiveOfkind(v) => write!(f, "FiveOfkind {}", *v),            
+        }
     }
 }
+
 
 fn compare_hands(hand_a: HandType, hand_b: HandType) -> std::cmp::Ordering {
-    if hand_a.cmp(&hand_b) != std::cmp::Ordering::Equal {
-        return hand_a.cmp(&hand_b);
-    }
+    let (left, right) = match (hand_a, hand_b) {
+        (HandType::HighCard(a), HandType::HighCard(b)) => (a, b),
+        (HandType::OnePair(a), HandType::OnePair(b)) => (a, b),
+        (HandType::TwoPair(a), HandType::TwoPair(b)) => (a, b),
+        (HandType::ThreeOfkind(a), HandType::ThreeOfkind(b)) => (a, b),
+        (HandType::FullHouse(a), HandType::FullHouse(b)) => (a, b),
+        (HandType::FourOfkind(a), HandType::FourOfkind(b)) => (a, b),
+        (HandType::FiveOfkind(a), HandType::FiveOfkind(b)) => (a, b),
+        // (HandType::FiveOfkind(_), _ ) => return std::cmp::Ordering::Greater,
+        // (HandType::FourOfkind(_), _ ) => return std::cmp::Ordering::Greater,
+        // (HandType::FullHouse(_), _ ) => return std::cmp::Ordering::Greater,
+        // (HandType::ThreeOfkind(_), _ ) => return std::cmp::Ordering::Greater,
+        // (HandType::TwoPair(_), _ ) => return std::cmp::Ordering::Greater,
+        // (HandType::OnePair(_), _ ) => return std::cmp::Ordering::Greater,
+        // (HandType::HighCard(_), _ ) => return std::cmp::Ordering::Greater,
+        _ => return hand_a.cmp(&hand_b)
+    };
 
-    match (hand_a, hand_b) {
-        (HandType::HighCard(a), HandType::HighCard(b)) => a.cmp(&b),
-        (HandType::OnePair(a), HandType::OnePair(b)) => a.cmp(&b),
-        (HandType::TwoPair(a1, a2), HandType::TwoPair(b1, b2)) => compare_two_value(a1, a2, b1, b2),
-        (HandType::ThreeOfkind(a), HandType::ThreeOfkind(b)) => a.cmp(&b),
-        (HandType::FullHouse(a1, a2), HandType::FullHouse(b1, b2)) => {
-            compare_two_value(a1, a2, b1, b2)
+    let left = left
+        .chars()
+        .map(|c| CARD_LABEL.get(&c).expect("Invalid card"))
+        .collect::<Vec<_>>();
+    let right = right
+        .chars()
+        .map(|c| CARD_LABEL.get(&c).expect("Invalid card"))
+        .collect::<Vec<_>>();
+
+    for (l, r) in zip(left, right) {
+        if l == r {
+            continue;
         }
-        (HandType::FourOfkind(a1, a2), HandType::FourOfkind(b1, b2)) => {
-            compare_two_value(a1, a2, b1, b2)
-        }
-        (HandType::FiveOfkind(a), HandType::FiveOfkind(b)) => a.cmp(&b),
-        _ => panic!("Not really similar!!!!"),
+        return l.cmp(r);
     }
+    std::cmp::Ordering::Equal
 }
 
-fn get_key_by_value(map: &HashMap<char, usize>, target_value: usize) -> Option<char> {
-    for (&key, &value) in map.iter() {
+fn is_nth_same_card(map: &HashMap<char, usize>, target_value: usize) -> bool {
+    for (&_key, &value) in map.iter() {
         if value == target_value {
-            return Some(key);
+            return true;
         }
     }
-    None
+    false
 }
 
 fn solve_hand(hand: &str) -> HandType {
@@ -80,82 +109,31 @@ fn solve_hand(hand: &str) -> HandType {
         *counter.entry(card).or_insert(0) += 1;
     }
 
-    let mut iter = counter.iter();
-    let c1 = iter.next();
-    let c2 = iter.next();
-    let c3 = iter.next();
-    let c4 = iter.next();
-    let c5 = iter.next();
-
     // Determinate the combination
     match counter.len() {
-        1 => {
-            let card = c1.expect("No value").0;
-            let value = CARD_LABEL.get(card).expect("Unknown value");
-            HandType::FiveOfkind(*value)
-        }
+        1 => HandType::FiveOfkind(hand),
         2 => {
-            let full_value = get_key_by_value(&counter, 4);
-            let three_value = get_key_by_value(&counter, 3);
-            let pair_value = get_key_by_value(&counter, 2);
+            let full_value = is_nth_same_card(&counter, 4);
+            let three_value = is_nth_same_card(&counter, 3);
+            let pair_value = is_nth_same_card(&counter, 2);
 
             match (full_value, three_value, pair_value) {
-                (Some(a), None, None) => HandType::FourOfkind(
-                    *CARD_LABEL.get(&a).unwrap(),
-                    *CARD_LABEL
-                        .get(&get_key_by_value(&counter, 1).unwrap())
-                        .unwrap(),
-                ),
-                (None, Some(a), Some(b)) => {
-                    let v1 = *CARD_LABEL.get(&a).unwrap();
-                    let v2 = *CARD_LABEL.get(&b).unwrap();
-                    HandType::FullHouse(max(v1, v2), min(v1, v2))
-                }
-                (None, None, Some(a)) => HandType::ThreeOfkind(*CARD_LABEL.get(&a).unwrap()),
-                _ => panic!("Match Hand type Full, Three or Pair"),
+                (true, false, false) => HandType::FourOfkind(hand),
+                (false, true, true) => HandType::FullHouse(hand),
+                (false, false, true) => HandType::ThreeOfkind(hand),
+                _ => unreachable!("Match Hand type Full, Three or Pair"),
             }
         }
         3 => {
-            if let Some(key) = get_key_by_value(&counter, 3) {
-                HandType::ThreeOfkind(*CARD_LABEL.get(&key).unwrap())
+            if is_nth_same_card(&counter, 3) {
+                HandType::ThreeOfkind(hand)
             } else {
-                let (a, b) = match (
-                    c1.expect("No value").1,
-                    c2.expect("No value").1,
-                    c3.expect("No value").1,
-                ) {
-                    (2, 2, 1) => (c1.expect("No value").0, c2.expect("No value").0),
-                    (2, 1, 2) => (c1.expect("No value").0, c3.expect("No value").0),
-                    (1, 2, 2) => (c3.expect("No value").0, c2.expect("No value").0),
-                    _ => panic!("Match Hand type Double Pair"),
-                };
-                let v1 = *CARD_LABEL.get(a).unwrap();
-                let v2 = *CARD_LABEL.get(b).unwrap();
-                HandType::TwoPair(max(v1, v2), min(v1, v2))
+                HandType::TwoPair(hand)
             }
         }
-        4 => {
-            if let Some(key) = get_key_by_value(&counter, 2) {
-                HandType::OnePair(*CARD_LABEL.get(&key).unwrap())
-            } else {
-                panic!("Match Hand type Pair")
-            }
-        }
-        5 => {
-            let greater = [
-                *CARD_LABEL.get(c1.unwrap().0).unwrap(),
-                *CARD_LABEL.get(c2.unwrap().0).unwrap(),
-                *CARD_LABEL.get(c3.unwrap().0).unwrap(),
-                *CARD_LABEL.get(c4.unwrap().0).unwrap(),
-                *CARD_LABEL.get(c5.unwrap().0).unwrap(),
-            ]
-            .iter()
-            .cloned()
-            .max()
-            .expect("Failed to find better hand");
-            HandType::HighCard(greater)
-        }
-        _ => panic!("Match Hand type failed"),
+        4 => HandType::OnePair(hand),
+        5 => HandType::HighCard(hand),
+        _ => unreachable!("Match Hand type failed"),
     }
 }
 
@@ -171,8 +149,6 @@ pub fn part_one(input: &str) -> Option<u32> {
         })
         .collect();
     hands.sort_by(|a, b| compare_hands(a.0, b.0));
-
-    println!("{:#?}",hands);
 
     Some(hands.iter().enumerate().fold(0, |acc, (range, (_, bid))| {
         acc + (range as u32 + 1) * (*bid)
@@ -190,7 +166,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(7830));
+        assert_eq!(result, Some(6440));
     }
 
     #[test]
@@ -201,45 +177,35 @@ mod tests {
 
     #[test]
     fn test_hand() {
-        assert_eq!(solve_hand("AAAAA"), HandType::FiveOfkind(14));
-        assert_eq!(solve_hand("AAAA2"), HandType::FourOfkind(14, 2));
-        assert_eq!(solve_hand("22223"), HandType::FourOfkind(2, 3));
-        assert_eq!(solve_hand("AAAKK"), HandType::FullHouse(14, 13));
-        assert_eq!(solve_hand("AAA32"), HandType::ThreeOfkind(14));
-        assert_eq!(solve_hand("77752"), HandType::ThreeOfkind(7));
-        assert_eq!(solve_hand("AA2KK"), HandType::TwoPair(14, 13));
-        assert_eq!(solve_hand("AA52T"), HandType::OnePair(14));
-        assert_eq!(solve_hand("23456"), HandType::HighCard(6));
-
         assert_eq!(
-            solve_hand("AAAAA").cmp(&solve_hand("KKKKK")),
+            compare_hands(solve_hand("AAAAA"), solve_hand("KKKKK")),
             std::cmp::Ordering::Greater
         );
         assert_eq!(
-            solve_hand("AAAAA").cmp(&solve_hand("AAAAA")),
+            compare_hands(solve_hand("AAAAA"), solve_hand("AAAAA")),
             std::cmp::Ordering::Equal
         );
         assert_eq!(
-            solve_hand("22222").cmp(&solve_hand("AAAAA")),
+            compare_hands(solve_hand("2AAAA"), solve_hand("3AAAA")),
             std::cmp::Ordering::Less
         );
 
         assert_eq!(
-            solve_hand("AAA22").cmp(&solve_hand("AAA66")),
+            compare_hands(solve_hand("AAA22"), solve_hand("AAA66")),
             std::cmp::Ordering::Less
         );
         assert_eq!(
-            solve_hand("AAK22").cmp(&solve_hand("AAK66")),
+            compare_hands(solve_hand("AAK22"), solve_hand("AAK66")),
             std::cmp::Ordering::Less
         );
 
         assert_eq!(
-            solve_hand("AA234").cmp(&solve_hand("TTKQ2")),
+            compare_hands(solve_hand("AA234"), solve_hand("TTKQ2")),
             std::cmp::Ordering::Greater
         );
 
         assert_eq!(
-            solve_hand("KK677").cmp(&solve_hand("KTJJT")),
+            compare_hands(solve_hand("KK677"), solve_hand("KTJJT")),
             std::cmp::Ordering::Greater
         );
     }
